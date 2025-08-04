49ba0959aacfa3bb72fefd44a0874e460beef99e
faldi28 <yohanespalis28@gmail.com>
2025-08-05 00:53:32 +0700

    QR code perbaikan

--- src/components/QRCodeDisplay.jsx
+++ src/components/QRCodeDisplay.jsx
@@ -7,7 +7,8 @@ function QRCodeDisplay({ registrationId }) {
       <h2>Registration Successful!</h2>
       <p>Show this QR code at the event for check-in.</p>
       {/* The QR code is generated based on a unique registration ID */}
-      <QRCodeSVG value={registrationId} size={256} /> {/* CHANGED THIS LINE */}
+      // SESUDAH
+      <QRCodeSVG value={`reg-${registrationId}`} size={256} />
       <p><strong>ID:</strong> {registrationId}</p>
     </div>
   );
